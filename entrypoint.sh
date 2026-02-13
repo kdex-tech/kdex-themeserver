d8e00521bb2ed322a43b71c9308f8e5bd1071138
Raymond Augé <rayauge@doublebite.com>
2026-02-13 11:28:14 -0500

    only dump public resources when instructed
    Signed-off-by: Raymond Augé <rayauge@doublebite.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-tree ${PUBLIC_RESOURCES_DIR:-/public}
+// if DUMP_PUBLIC_RESOURCES is set, dump the public resources directory
+if [ ! -z "${DUMP_PUBLIC_RESOURCES}" ]; then
+    tree ${PUBLIC_RESOURCES_DIR:-/public}
+fi
 
 caddy run --adapter caddyfile --config /etc/caddy/Caddyfile
